23657f18dbf29260a2952737c38b0049feb65ca3
Ryan <tripper@TripperdeMacBook-Pro.local>
2016-12-15 01:52:20 +0800

    evaluate gf-w2v model

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import random
 import subprocess
 import re
 import time
+import json
 # jieba.set_dictionary('data/DICT_CK+jieba_lower')
 # jieba.add_word('龐燮傍謝', freq=10, tag='xx')
 ANS = ['a', 'b', 'c', 'd', 'e']
@@ -44,6 +45,7 @@ class TokenQuestion(Question):
         super().__init__(row)
 
         self.tokenizer = tokenizer
+        if 'cached' in kwargs: self.cached = kwargs['cached']
         self.tokenize()
 
 
@@ -60,7 +62,15 @@ class TokenQuestion(Question):
             self.wlist = wlist
             self.qidx = qidx
         elif self.tokenizer == 'guofoo':
-            print('[tokenizer]oh... you forgot something')
+            wlist = re.sub(r'\s+', ' ', re.sub('︽⊙＿⊙︽', ' ︽⊙＿⊙︽ ', context)).split()
+            qidx = []
+            for i, w in enumerate(wlist):
+                if w == '︽⊙＿⊙︽':
+                    wlist[i] = '*'
+                    qidx.append(i)
+            self.wlist = wlist
+            self.qidx = qidx
+
         elif self.tokenizer == 'uni':
             context = context.replace('︽⊙＿⊙︽', '*')
             wlist = re.sub(r'\s+',' ', re.sub(r'([\u4E00-\u9FFF\*])', r' \1 ', context)).strip().split(' ')
@@ -69,6 +79,15 @@ class TokenQuestion(Question):
                 if w == '*': qidx.append(i)
             self.wlist = wlist
             self.qidx = qidx
+        
+        elif self.tokenizer == 'cached':
+            wlist =  re.sub(r'\s+',' ', self.cached.replace('︽⊙＿⊙︽', '*')).strip().split()
+            qidx = []
+            for i, w in enumerate(wlist):
+                if w == '*': qidx.append(i)
+            self.wlist = wlist
+            self.qidx = qidx
+
 
 
 class Solver(object):
@@ -125,14 +144,23 @@ class Word2VecSolver(Solver):
         
         self.model = gensim.models.Word2Vec.load_word2vec_format(path + prefix + '-syn0.bin', binary = True, unicode_errors = 'ignore')
         self.vocab_size, self.vector_size = self.model.syn0.shape
-        syn1neg = np.zeros((self.vocab_size, self.vector_size), dtype = np.float32)
+        
 
         binary_len = np.dtype(np.float32).itemsize * self.vector_size
-        with smart_open(path + prefix + '-syn1neg.bin') as fin:
-            for i in range(self.vocab_size):
-                weights = np.fromstring(fin.read(binary_len), dtype=np.float32)
-                syn1neg[i] = weights
-        self.syn1neg = syn1neg
+        if 'skip' in prefix: 
+            syn1 = np.zeros((self.vocab_size, self.vector_size), dtype = np.float32)
+            with smart_open(path + prefix + '-syn1.bin') as fin:
+                for i in range(self.vocab_size):
+                    weights = np.fromstring(fin.read(binary_len), dtype=np.float32)
+                    syn1[i] = weights
+            self.syn1 = syn1
+        elif 'cbow' in prefix:
+            syn1neg = np.zeros((self.vocab_size, self.vector_size), dtype = np.float32)
+            with smart_open(path + prefix + '-syn1neg.bin') as fin:
+                for i in range(self.vocab_size):
+                    weights = np.fromstring(fin.read(binary_len), dtype=np.float32)
+                    syn1neg[i] = weights
+            self.syn1neg = syn1neg
 
         super().__init__()
 
@@ -154,10 +182,10 @@ class Word2VecSolver(Solver):
                 ret.append(re.sub(r'\s+',' ', re.sub(r'([\u4E00-\u9FFF])', r' \1 ', opt)).strip().split(' ')) 
         else:
             for opt in options:
-                ret.append([opt]) 
+                ret.append(opt.split()) 
         return ret
 
-    def solve(self, tokenized_question, uni_gram = False, key = None):
+    def solve(self, tokenized_question, solver = 'syn1neg', uni_gram = False, key = None):
         q = tokenized_question
         num_opt = len(q.options)
 
@@ -190,7 +218,16 @@ class Word2VecSolver(Solver):
                                 for v in others:
                                     if v >= 0: 
                                         temp += self.model.syn0[v]
-                                s += np.dot(normalize_vec(temp), normalize_vec(self.syn1neg[w]))
+                                if 'cbow' in self.prefix:
+                                    if solver == 'syn0':
+                                        s += np.dot(normalize_vec(temp), normalize_vec(self.model.syn0[w]))
+                                    elif solver == 'syn1neg':
+                                        s += np.dot(normalize_vec(temp), normalize_vec(self.syn1neg[w]))
+                                    elif solver == 'both':
+                                        temp2 = normalize_vec(temp)
+                                        s += (np.dot(temp2, normalize_vec(self.syn1neg[w])) + np.dot(temp2, normalize_vec(self.model.syn0[w])))
+                                elif 'skip' in self.prefix: pass
+
                                 k += 1
 
                         score[i] += float(s) / float(max(k, 1))
@@ -199,6 +236,8 @@ class Word2VecSolver(Solver):
                 self.options_prob = {x: y for x, y in zip(string.ascii_lowercase[:num_opt], score)}
                 self.prediction = string.ascii_lowercase[score.index(max(score))]
             else:
+                print('!!!')
+                print(q.context, q.qidx, q.options)
                 self.prediction = Solver.UNSOLVED
 
 
@@ -260,11 +299,27 @@ class RetrievalSolver(Solver):
                     self.options_prob = {x: 0 for x in string.ascii_lowercase[:num_opt]}
                     self.options_prob[self.prediction] = 1
                     break
-
-
         return self.prediction
 
 
+class GrepClassifier(object):
+    def __init__(self, context_path):
+        self.context_path = context_path
+    def predict(sentence):
+        context_path = 'clean-qa-jq.jsonl'
+        cleaned = re.sub(r'([\.\^\$\|\*\+\?\\\{\}\[\]\(\)])', r'\\\1', re.sub(r'"(.)', r'\1', sentence))
+        seg_str = cleaned.split('︽⊙＿⊙︽')
+        if len(seg_str) < 2: return 'unkown'
+        seg_len = [len(s) for s in seg_str]
+        seg_num = len(seg_str)
+        long_idx = seg_len.index(max(seg_len))
+        long_str = seg_str[long_idx]       
+        cmd = "grep -F '{long_str}' {file}".format(file = self.context_path, long_str=long_str)
+        category = 'unkown'
+        try:
+            category = json.loads(subprocess.run(cmd, shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8').strip())['category']
+        except: pass
+        print(category)
 
 import html
 from urllib.parse import quote
